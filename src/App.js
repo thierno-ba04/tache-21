dd1bb75d816097f3c1a0ad51799dd5f11bf44224
faty niang <fatyniang128@gmail.com>
2024-07-24 16:58:08 +0100

    TRVAIL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DashboardEtudiant from './pagesEtudiant/dashboard/DashboardEtudiant';
 import LayoutEtudiant from './layout/LayoutEtudiant';
