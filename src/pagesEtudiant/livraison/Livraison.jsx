216f90598373c32355a1af83448231967851adf6
Thierno-ba04 <thiernoba365@gmail.com>
2024-07-26 13:33:53 +0000

    first commit

--- src/pagesEtudiant/livraison/Livraison.jsx
+++ src/pagesEtudiant/livraison/Livraison.jsx
@@ -1,9 +1,260 @@
+import { Col, Container, Row } from "react-bootstrap";
+import "./livraison.css";
+
+import capture1 from "../../assets/img/164153.png";
+
 const Livraison = () => {
-    return ( 
-        <div>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos quisquam illo voluptatibus quaerat qui voluptates delectus eveniet necessitatibus sapiente numquam, error inventore sunt alias earum? Quae ab assumenda et ducimus.</p>
-        </div>
-     );
-}
- 
+  return (
+    <div className="merelivraison">
+      <Container>
+        <Row>
+          <Col lg={4} md={6}>
+            <div className="mere-cards">
+              <div className="card" style={{ width: "18rem" }}>
+                <img src={capture1} alt="" />
+                <div className="card-body">
+                  <h5 className="card-title">Tache N1</h5>
+                  <p className="card-text">
+                    Tâche 01 : Les bases du HTML5 et
+                  </p>
+                  <div className="d-flex gap-3">
+                    <div className="btn-commentaire">
+                      <a href="#" className="btn btn-primary">
+                        commentaire
+                      </a>
+                    </div>
+                    <div className="btn-livrables">
+                      <a href="#" className="btn btn-success btn-livrables">
+                        livrables
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+
+          <Col lg={4} md={6}>
+            <div className="mere-cards">
+              <div className="card" style={{ width: "18rem" }}>
+                <img src={capture1} alt="" />
+                <div className="card-body">
+                  <h5 className="card-title">Tache N1</h5>
+                  <p className="card-text">
+                    Tâche 01 : Les bases du HTML5 et
+                  </p>
+                  <div className="d-flex gap-3">
+                    <div className="btn-commentaire">
+                      <a href="#" className="btn btn-primary">
+                        commentaire
+                      </a>
+                    </div>
+                    <div className="btn-livrables">
+                      <a href="#" className="btn btn-success btn-livrables">
+                        livrables
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+
+
+          <Col lg={4} md={6}>
+            <div className="mere-cards">
+              <div className="card" style={{ width: "18rem" }}>
+                <img src={capture1} alt="" />
+                <div className="card-body">
+                  <h5 className="card-title">Tache N1</h5>
+                  <p className="card-text">
+                    Tâche 01 : Les bases du HTML5 et
+                  </p>
+                  <div className="d-flex gap-3">
+                    <div className="btn-commentaire">
+                      <a href="#" className="btn btn-primary">
+                        commentaire
+                      </a>
+                    </div>
+                    <div className="btn-livrables">
+                      <a href="#" className="btn btn-success btn-livrables">
+                        livrables
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+         
+        </Row>
+
+        <Row>
+          <Col lg={4} md={6}>
+            <div className="mere-cards">
+              <div className="card" style={{ width: "18rem" }}>
+                <img src={capture1} alt="" />
+                <div className="card-body">
+                  <h5 className="card-title">Tache N1</h5>
+                  <p className="card-text">
+                    Tâche 01 : Les bases du HTML5 et
+                  </p>
+                  <div className="d-flex gap-3">
+                    <div className="btn-commentaire">
+                      <a href="#" className="btn btn-primary">
+                        commentaire
+                      </a>
+                    </div>
+                    <div className="btn-livrables">
+                      <a href="#" className="btn btn-success btn-livrables">
+                        livrables
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+
+          <Col lg={4} md={6}>
+            <div className="mere-cards">
+              <div className="card" style={{ width: "18rem" }}>
+                <img src={capture1} alt="" />
+                <div className="card-body">
+                  <h5 className="card-title">Tache N1</h5>
+                  <p className="card-text">
+                    Tâche 01 : Les bases du HTML5 et
+                  </p>
+                  <div className="d-flex gap-3">
+                    <div className="btn-commentaire">
+                      <a href="#" className="btn btn-primary">
+                        commentaire
+                      </a>
+                    </div>
+                    <div className="btn-livrables">
+                      <a href="#" className="btn btn-success btn-livrables">
+                        livrables
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+
+
+          <Col lg={4} md={6}>
+            <div className="mere-cards">
+              <div className="card" style={{ width: "18rem" }}>
+                <img src={capture1} alt="" />
+                <div className="card-body">
+                  <h5 className="card-title">Tache N1</h5>
+                  <p className="card-text">
+                    Tâche 01 : Les bases du HTML5 et
+                  </p>
+                  <div className="d-flex gap-3">
+                    <div className="btn-commentaire">
+                      <a href="#" className="btn btn-primary">
+                        commentaire
+                      </a>
+                    </div>
+                    <div className="btn-livrables">
+                      <a href="#" className="btn btn-success btn-livrables">
+                        livrables
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+         
+        </Row>
+
+        <Row className="">
+          <Col lg={4} md={6}>
+            <div className="mere-cards">
+              <div className="card" style={{ width: "18rem" }}>
+                <img src={capture1} alt="" />
+                <div className="card-body">
+                  <h5 className="card-title">Tache N1</h5>
+                  <p className="card-text">
+                    Tâche 01 : Les bases du HTML5 et
+                  </p>
+                  <div className="d-flex gap-3">
+                    <div className="btn-commentaire">
+                      <a href="#" className="btn btn-primary">
+                        commentaire
+                      </a>
+                    </div>
+                    <div className="btn-livrables">
+                      <a href="#" className="btn btn-success btn-livrables">
+                        livrables
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+
+          <Col lg={4} md={6}>
+            <div className="mere-cards">
+              <div className="card" style={{ width: "18rem" }}>
+                <img src={capture1} alt="" />
+                <div className="card-body">
+                  <h5 className="card-title">Tache N1</h5>
+                  <p className="card-text">
+                    Tâche 01 : Les bases du HTML5 et
+                  </p>
+                  <div className="d-flex gap-3">
+                    <div className="btn-commentaire">
+                      <a href="#" className="btn btn-primary">
+                        commentaire
+                      </a>
+                    </div>
+                    <div className="btn-livrables">
+                      <a href="#" className="btn btn-success btn-livrables">
+                        livrables
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+
+
+          <Col lg={4} md={6}>
+            <div className="mere-cards">
+              <div className="card" style={{ width: "18rem" }}>
+                <img src={capture1} alt="" />
+                <div className="card-body">
+                  <h5 className="card-title">Tache N1</h5>
+                  <p className="card-text">
+                    Tâche 01 : Les bases du HTML5 et
+                  </p>
+                  <div className="d-flex gap-3">
+                    <div className="btn-commentaire">
+                      <a href="#" className="btn btn-primary">
+                        commentaire
+                      </a>
+                    </div>
+                    <div className="btn-livrables">
+                      <a href="#" className="btn btn-success btn-livrables">
+                        livrables
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+         
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
 export default Livraison;
