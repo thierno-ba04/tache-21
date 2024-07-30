41fd903f5dc6217dd5a110f4c5280bbcfae2d82d
Thierno-ba04 <thiernoba365@gmail.com>
2024-07-30 10:22:18 +0000

    first commit

--- src/pagesEtudiant/dashboard/DashboardEtudiant.jsx
+++ src/pagesEtudiant/dashboard/DashboardEtudiant.jsx
@@ -1,15 +1,58 @@
-// import SidebarEtudiant from "../components/SidebarEtudiant";
-
+import { Col, Container, Row } from "react-bootstrap";
 import SidebarEtudiant from "../../components/SidebarEtudiant";
+import { MdOutlineDoneOutline, MdOutlineSupervisorAccount } from "react-icons/md";
+import "./dashboard.css";
+import { FaFileAlt } from "react-icons/fa";
+import { BsBook } from "react-icons/bs";
 
 const DashboardEtudiant = () => {
-    return ( 
-        <div>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel quam quia a suscipit omnis veritatis. Consequuntur reprehenderit architecto, quae, dolore beatae cum dolorem tempore nemo rerum praesentium consectetur enim eaque!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil rem voluptate modi laudantium officiis quod dolores, expedita recusandae facere facilis explicabo dolorem. Delectus dolores similique atque provident? Atque, nam culpa?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, delectus aliquid? Dolorum provident ullam, ipsam ducimus maxime voluptatum suscipit reprehenderit veritatis sed, minima et vitae tempora, architecto quidem. Quas, commodi.
-        </div>
-     );
-}
- 
+  return (
+    <div className="main">
+      <Container>
+        <Row>
+          <Col lg={4} md={4}>
+            <div className="main-card">
+              <h6>Année Scolaire 2020/2021</h6>
+              <div className="div_mer">
+                <div className="div-1">
+                  <h5>TACHE</h5>
+                  <p>Nombre de toutes les tache</p>
+                  <h1>69</h1>
+                  <span className="icones-perso">
+                    <BsBook size={50} />
+                  </span>
+                </div>
+                <div className="border"></div>
+                <div className="div-2">
+                  <h2>69</h2>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col lg={4} md={4}>
+            <div className="main-card main-card2">
+              <h5>Taches Validées</h5>
+              <p>Nombres de Tache validées</p>
+              <h4>37/68</h4>
+              <span className="icones-perso2">
+                <MdOutlineDoneOutline size={55} />
+              </span>
+            </div>
+          </Col>
+          <Col lg={4} md={4}>
+            <div className="main-card main-card2">
+              <h5>Programme</h5>
+              <p>Liste Programme</p>
+              <h4>6</h4>
+              <span className="icones-perso2">
+                <FaFileAlt size={55} />
+              </span>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
 export default DashboardEtudiant;
--- src/pagesEtudiant/tache/Tache.jsx
+++ src/pagesEtudiant/tache/Tache.jsx
@@ -3,10 +3,7 @@ import "./tache.css";
 
 const Tache = () => {
   return (
-    <div
-      className="mere-tache"
-      style={{ marginTop: "80px , paddingTop: 120px" }}
-    >
+    <div className="mere-tache" style={{ marginTop: "80px", paddingTop: "120px" }}>
       {/* <div className="d-flex ">
         <div className="">
             <p>DASHBOARD / TACHE</p>
@@ -23,24 +20,25 @@ const Tache = () => {
                 Tache N:1
                 <p>durée: 2h</p>
                 <h6>Syllabus</h6>
-                <p>HTML/CSS_2024</p> <br /> <br />
+                <p>HTML/CSS_2024</p>
+                <br />
                 <br />
                 <p>démarée il y a un an</p>
               </div>
               <div className="ms-3 mt-3">
                 <h5 className="h5tache">Tache:00</h5>
                 <p>
-                  1. Vous créez un dossier sur Google Drive nommé Prénom Nom .
+                  1. Vous créez un dossier sur Google Drive nommé Prénom Nom.
                   Vous créez à l’intérieur un dossier pour chaque projet. Vous y
                   mettrez vos captures d’écran et vos réalisations par la suite.
                   Vous partagez le dossier avec le mail
                   contact.elearnign@gmail.com.
-                </p>{" "}
+                </p>
                 <br />
                 <br />
                 <div className="lienutiles">
                   <p className="p-lien">
-                    Liens utiles : <br />{" "}
+                    Liens utiles : <br />
                   </p>
                   <a href="https://drive.google.com/drive/my-drive">
                     https://drive.google.com/drive/my-drive
@@ -66,7 +64,8 @@ const Tache = () => {
                 Tache N:2
                 <p>durée: 2h</p>
                 <h6>Syllabus</h6>
-                <p>HTML/CSS_2024</p> <br /> <br />
+                <p>HTML/CSS_2024</p>
+                <br />
                 <br />
               </div>
               <div className="ms-3 mt-3">
@@ -74,20 +73,19 @@ const Tache = () => {
                   Tâche 01 : Les bases du HTML5 et CSS3
                 </h5>
                 <p>
-                  Faire des recherches sur les éditeurs de textes Suivre ce
+                  Faire des recherches sur les éditeurs de textes. Suivre ce
                   tutoriel et reproduire soigneusement tous les exercices sur
-                  votre éditeur <p className="deplace">de texte.</p>{" "}
+                  votre éditeur de texte.
                 </p>
                 <br />
                 <br />
                 <div className="lienutiles">
                   <p className="p-lien">
-                    Liens utiles : <br />{" "}
+                    Liens utiles : <br />
                   </p>
                   <a
                     className="a-tache1"
-                    href="https://www.youtube.com/playlist?list=PLwLsbqvBlImHG5yeUCXJ1aqNMgUKi1NK3
-"
+                    href="https://www.youtube.com/playlist?list=PLwLsbqvBlImHG5yeUCXJ1aqNMgUKi1NK3"
                   >
                     https://www.youtube.com/playlist?list=PLwLsbqvBlImHG5yeUCXJ1aqNMgUKi1NK3
                   </a>
@@ -109,31 +107,31 @@ const Tache = () => {
           <Col lg={10} md={12}>
             <div className="bg-tache d-md-flex ">
               <div className="borderReight ms-4 mt-3">
-                Tache N:2
+                Tache N:3
                 <p>durée: 2h</p>
                 <h6>Syllabus</h6>
-                <p>HTML/CSS_2024</p> <br /> <br />
+                <p>HTML/CSS_2024</p>
+                <br />
                 <br />
               </div>
               <div className="ms-3 mt-3">
                 <h5 className="h5tache1">
-                  Tâche 01 : Les bases du HTML5 et CSS3
+                  Tâche 02 : Les bases du HTML5 et CSS3
                 </h5>
                 <p>
-                  Faire des recherches sur les éditeurs de textes Suivre ce
+                  Faire des recherches sur les éditeurs de textes. Suivre ce
                   tutoriel et reproduire soigneusement tous les exercices sur
-                  votre éditeur <p className="deplace">de texte.</p>{" "}
+                  votre éditeur de texte.
                 </p>
                 <br />
                 <br />
                 <div className="lienutiles">
                   <p className="p-lien">
-                    Liens utiles : <br />{" "}
+                    Liens utiles : <br />
                   </p>
                   <a
                     className="a-tache1"
-                    href="https://www.youtube.com/playlist?list=PLwLsbqvBlImHG5yeUCXJ1aqNMgUKi1NK3
-"
+                    href="https://www.youtube.com/playlist?list=PLwLsbqvBlImHG5yeUCXJ1aqNMgUKi1NK3"
                   >
                     https://www.youtube.com/playlist?list=PLwLsbqvBlImHG5yeUCXJ1aqNMgUKi1NK3
                   </a>
